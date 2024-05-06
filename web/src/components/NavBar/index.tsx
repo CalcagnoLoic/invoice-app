7b44caac1776bce9b14e04d2ffb0c9753e11450b
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-05-06 12:54:48 +0200

    ending navbar

--- web/src/components/NavBar/index.tsx
+++ web/src/components/NavBar/index.tsx
@@ -1,7 +1,24 @@
+import NavBarIcons from "../../icons/NavBarIcons";
+import PROFILE from "../../../public/assets/img/photo.webp";
+
 const Component = () => {
   return (
-    <nav className="bg-oxfordBlue h-20 w-full xl:h-screen xl:w-24 xl:rounded-r-3xl">
-      <div></div>
+    <nav className="bg-oxfordBlue flex h-[103px] w-full justify-between xl:h-screen xl:w-[103px] xl:flex-col xl:rounded-r-3xl">
+      <div>
+        <NavBarIcons kind="background" />
+      </div>
+
+      <div className="flex xl:flex-col">
+        <NavBarIcons kind="moon" />
+
+        <div className="border-fiord flex h-full items-center self-center border-l-2 xl:w-full xl:border-l-0 xl:border-t-2">
+          <img
+            src={PROFILE}
+            alt="My Profile"
+            className="mx-6 my-auto h-10 w-10 rounded-full xl:mx-auto xl:my-6"
+          />
+        </div>
+      </div>
     </nav>
   );
 };
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -15,7 +15,8 @@ export default {
         burntSienna: "#EC5757",
         monaLisa: "#FF9797",
         whisper: "#F8F8FB",
-        oxfordBlue: "#373B53"
+        oxfordBlue: "#373B53",
+        fiord: "#494E6E"
       },
       fontFamily: {
         spartan: ["spartan"]
